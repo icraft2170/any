11973194aafa432d7acf8a65388a95ba5b85162d
Hero <icraft2170@gmail.com>
2022-10-10 00:04:19 +0900

    selection sort - add description

--- sort_algorithm/src/main/java/SelectionSort.java
+++ sort_algorithm/src/main/java/SelectionSort.java
@@ -7,7 +7,9 @@ public class SelectionSort implements Sort{
 
     // 왼쪽 맨 끝부터 각 인덱스에 알 맞은 값을 넣기 위해 순회한다.
     for (int i = 0; i < length; i++) {
+      // 가장 작은 값을 가진 인덱스를 기록해둔다.
       int min = i;
+      // 시작 인덱스부터 배열의 끝까지 순회를 돌면서 가장 작은 값의 인덱스를 찾고 시작 값과 교체한다.
       for (int j = i; j < length; j++) {
         if (array[min] == array[j]) continue;
         if (array[min] > array[j]) min = j;
