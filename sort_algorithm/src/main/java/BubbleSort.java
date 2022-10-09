1b97b03a210297484131ff5ce49d701406e276fe
Hero <icraft2170@gmail.com>
2022-10-09 23:46:44 +0900

    add bubble sort

--- sort_algorithm/src/main/java/BubbleSort.java
+++ sort_algorithm/src/main/java/BubbleSort.java
@@ -3,10 +3,18 @@ public class BubbleSort implements Sort {
   @Override
   public int[] sorting(int[] array) {
     int length = array.length;
-    if (length == 1) return array;
+    if (length < 2) return array;
 
+    for (int i = length - 1; i > 0; i--) {
+      for (int j = 0; j < i; j++) {
+        if (array[j] > array[j + 1]) {
+          int temp = array[j + 1];
+          array[j + 1] = array[j];
+          array[j] = temp;
+        }
+      }
+    }
 
-
-    return new int[0];
+    return array;
   }
 }
--- sort_algorithm/src/main/java/Main.java
+++ sort_algorithm/src/main/java/Main.java
@@ -1,7 +1,14 @@
 public class Main {
 
   public static void main(String[] args) {
-//      Sort sort = new
+    Sort sort = new BubbleSort();
+
+    int[] array = new int[] {6, 3, 2, 9, 7};
+    int[] sortedArray = sort.sorting(array);
+
+    for (int i : sortedArray) {
+      System.out.print(i + " ");
+    }
   }
 
 }
