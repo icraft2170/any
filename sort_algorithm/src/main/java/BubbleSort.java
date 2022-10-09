b5246fb62888b956717e5fef8a1c87e049293934
Hero <icraft2170@gmail.com>
2022-10-09 23:37:17 +0900

    add sort algorithm project

---
+++ sort_algorithm/src/main/java/BubbleSort.java
@@ -0,0 +1,12 @@
+
+public class BubbleSort implements Sort {
+  @Override
+  public int[] sorting(int[] array) {
+    int length = array.length;
+    if (length == 1) return array;
+
+
+
+    return new int[0];
+  }
+}
