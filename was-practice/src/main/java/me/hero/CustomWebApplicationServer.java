affad8a87ddaf969c7b7e47cd3820782b74ddb48
Hero <icraft2170@gmail.com>
2022-10-28 23:55:12 +0900

    WAS Thread Create

--- was-practice/src/main/java/me/hero/CustomWebApplicationServer.java
+++ was-practice/src/main/java/me/hero/CustomWebApplicationServer.java
@@ -10,6 +10,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import me.hero.calculator.Calculator;
+import me.hero.calculator.ClientRequestHandler;
 import me.hero.calculator.HttpRequest;
 import me.hero.calculator.operator.PositiveNumber;
 import org.slf4j.Logger;
@@ -34,29 +35,7 @@ public class CustomWebApplicationServer {
 
       while ((clientSocket = serverSocket.accept()) != null) {
         logger.info("[Custom Web Application Server] client Connected");
-
-        try(InputStream in = clientSocket.getInputStream(); OutputStream out = clientSocket.getOutputStream()) {
-          BufferedReader br =new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-          DataOutputStream dos = new DataOutputStream(out);
-
-          HttpRequest httpRequest = new HttpRequest(br);
-
-          if (httpRequest.isGetRequest() && httpRequest.matchPath("/calculate")) {
-            QueryStrings queryStrings = httpRequest.getQueryStrings();
-
-            int operand1 = Integer.parseInt(queryStrings.getValue("operand1"));
-            String operator = queryStrings.getValue("operator");
-            int operand2 = Integer.parseInt(queryStrings.getValue("operand2"));
-
-            int result = Calculator.calculate(new PositiveNumber(operand1), operator,
-                new PositiveNumber(operand2));
-            byte[] body = String.valueOf(result).getBytes();
-
-            HttpResponse response = new HttpResponse(dos);
-            response.response200Header("application/json", body.length);
-            response.responseBody(body);
-          }
-        }
+        new Thread(new ClientRequestHandler(clientSocket)).start();
       }
     }
   }
